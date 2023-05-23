33f27ace1f4c9b3f792058e70fcee0373fafed2f
Cholpon <cholponishenbek03@gmail.com>
2023-05-23 12:15:07 +0600

    timer with start and reset buttons are created

--- src/main/java/com/example/justtodoit/JustDoItController.java
+++ src/main/java/com/example/justtodoit/JustDoItController.java
@@ -7,6 +7,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.util.Duration;
 
 import java.util.EventListener;
 import java.util.Timer;
@@ -45,49 +53,93 @@ public class JustDoItController {
     @FXML
     private Button breakBtn;
 
-    private int sessiontime = 1500;
-    private int elapsetime = sessiontime * 1000;
-    private int h = elapsetime/3600000;
-    private int m = (elapsetime/60000) % 60;
-    private int s = (elapsetime/1000) % 60;
-    boolean startClicked = false;
+    private int sessionTime = 1500;
+    private int elapsedSeconds = sessionTime;
+    private Timeline timer;
+    private boolean startClicked = false;
 
-    String h_string = String.format("%02d", h);
-    String m_string = String.format("%02d", m);
-    String s_string = String.format("%02d", s);
+    public void initialize() {
+        updateTimeLabel();
 
-    Timer timer = new Timer(1000, new ActionListener(){
-        @Override
-        public void actionPerformed(ActionEvent e){
-            if(elapsetime != 0){
-                elapsetime = elapsetime - 1000;
+        timer = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            if (elapsedSeconds > 0) {
+                elapsedSeconds--;
+                updateTimeLabel();
+            } else {
+                stopTimerInZero();
             }
-        }
-    });
-
-//    @FXML
-//    void handleDoneButton(MouseEvent event) {
-//
-//    }
-//    @FXML
-//    private void handlePomoButton() {
-//        time1.setVisible(true);
-//        time2.setVisible(false);
-//        pomoBtn.setStyle("-fx-background-color: #94bbe9");
-//    }
-//
-//    @FXML
-//    private void handleBreakButton() {
-//        time1.setVisible(false);
-//        time2.setVisible(true);
-//        breakBtn.setStyle("-fx-background-color: #94bbe9");
-//    }
-
-    @FXML
-    void initialize() {
-        time1.setText(h_string + ":" + m_string + ":" + s_string);
+        }));
+        timer.setCycleCount(Animation.INDEFINITE);
 
     }
+
+    @FXML
+    private void handleStartButton() {
+        if (!startClicked) {
+            startBtn.setText("STOP");
+            startClicked = true;
+            timer.play();
+        } else {
+            startBtn.setText("START");
+            startClicked = false;
+            timer.pause();
+        }
+    }
+
+    @FXML
+    private void handleResetButton() {
+        timer.stop();
+        elapsedSeconds = sessionTime;
+        updateTimeLabel();
+        startBtn.setText("START");
+        startClicked = false;
+    }
+
+    private void updateTimeLabel() {
+        int hours = elapsedSeconds / 3600;
+        int minutes = (elapsedSeconds % 3600) / 60;
+        int seconds = elapsedSeconds % 60;
+
+        String hString = String.format("%02d", hours);
+        String mString = String.format("%02d", minutes);
+        String sString = String.format("%02d", seconds);
+
+        time1.setText(hString + ":" + mString + ":" + sString);
+    }
+
+    private void stopTimerInZero() {
+        timer.stop();
+        elapsedSeconds = sessionTime;
+        updateTimeLabel();
+        startBtn.setText("START");
+        startClicked = false;
+
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Session End");
+        alert.setHeaderText("Nice Session");
+
+        ButtonType continueButtonType = new ButtonType("Continue");
+        ButtonType cancelButtonType = new ButtonType("Cancel");
+
+        alert.getButtonTypes().setAll(continueButtonType, cancelButtonType);
+
+        alert.showAndWait().ifPresent(buttonType -> {
+            if (buttonType == continueButtonType) {
+                showAlert("Keep Going");
+            } else if (buttonType == cancelButtonType) {
+                showAlert("Don't Give Up");
+            }
+        });
+    }
+
+    private void showAlert(String message) {
+        Alert alert1 = new Alert(AlertType.INFORMATION);
+        alert1.setTitle("Action Taken");
+        alert1.setHeaderText(null);
+        alert1.setContentText("You chose to: " + message);
+        alert1.showAndWait();
+    }
+
 
 }
 
